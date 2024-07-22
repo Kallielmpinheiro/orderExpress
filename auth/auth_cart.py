97a2cbfc1dd9d5cd44153414a918bf8f0e7e124c
Kalliel <Kallielmpinheiro2004@gmail.com>
2024-07-22 10:44:33 -0300

    config : Refatorando classes e rotas

--- auth/auth_cart.py
+++ auth/auth_cart.py
@@ -1,39 +1,36 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, redirect, url_for, flash
 from flask_login import login_required
 from classes.product import Product
 from classes.cartOrder import Cart
 
 cart_bp = Blueprint('cart', __name__)
 
-@cart_bp.route('/cart')
+@cart_bp.route('/carrinho')
 @login_required
-def viewCart():
-    cart_items = Cart.getCartItems()
-    total_price = sum(item['price'] for item in cart_items)
-    return render_template('cart.html', cart_items=cart_items, total_price=total_price)
+def exibirCarrinho():
+    return Cart.renderizarCarrinho()
 
-@cart_bp.route('/add_to_cart/<product_name>')
+@cart_bp.route('/adicionar_ao_carrinho/<product_name>')
 @login_required
-def addCart(product_name):
+def adicionarAoCarrinho(product_name):
     product = next((p for p in Product.getByProducts() if p.nome == product_name), None)
     if product:
-        Cart.addCart(product)
+        Cart.adicionarItem(product)
         flash(f"Produto {product_name} adicionado ao carrinho!")
     else:
         flash(f"Produto {product_name} não encontrado.")
     return redirect(url_for('user.index'))
 
-@cart_bp.route('/remove_from_cart/<product_name>')
+@cart_bp.route('/remover_do_carrinho/<product_name>')
 @login_required
-def removeCart(product_name):
-    Cart.removeCart(product_name)
+def removerDoCarrinho(product_name):
+    Cart.removerItem(product_name)
     flash(f"Produto {product_name} removido do carrinho!")
-    return redirect(url_for('cart.viewCart'))
+    return redirect(url_for('cart.exibirCarrinho'))
 
-@cart_bp.route('/clear_cart')
+@cart_bp.route('/limpar_carrinho')
 @login_required
-def clearCart():
-    Cart.clearCart()
+def limparCarrinho():
+    Cart.limpar()
     flash("Carrinho esvaziado!")
-    return redirect(url_for('cart.viewCart'))
-    
+    return redirect(url_for('cart.exibirCarrinho'))
--- classes/cartOrder.py
+++ classes/cartOrder.py
@@ -1,28 +1,35 @@
-from flask import session
+from flask import session, render_template
 
 class Cart:
     @staticmethod
-    def initializeCart():
+    def inicializarCarrinho():
         if 'cart' not in session:
             session['cart'] = []
 
     @staticmethod
-    def addCart(product):
-        Cart.initializeCart()
-        session['cart'].append(product.dict())
+    def adicionarItem(produto):
+        Cart.inicializarCarrinho()
+        session['cart'].append(produto.dict())
         session.modified = True
 
     @staticmethod
-    def removeCart(product_name):
-        Cart.initializeCart()
-        session['cart'] = [item for item in session['cart'] if item['nome'] != product_name]
+    def removerItem(nomeProduto):
+        Cart.inicializarCarrinho()
+        session['cart'] = [item for item in session['cart'] if item['nome'] != nomeProduto]
         session.modified = True
 
     @staticmethod
-    def getCartItems():
-        Cart.initializeCart()
+    def obterItens():
+        Cart.inicializarCarrinho()
         return session['cart']
 
     @staticmethod
-    def clearCart():
+    def limpar():
         session.pop('cart', None)
+        session.modified = True
+
+    @staticmethod
+    def renderizarCarrinho():
+        cart_items = Cart.obterItens()
+        total_price = sum(item['price'] for item in cart_items)
+        return render_template('cart.html', cart_items=cart_items, total_price=total_price)
